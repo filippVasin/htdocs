657f57a938e025da81e2611a20dac018840fa7c5
filippVasin <vasin.filipp@yandex.ru>
2017-12-07 09:40:14 +0700

    непоказывать мед осмотр Диспетчеру

--- application/models/creator.php
+++ application/models/creator.php
@@ -378,7 +378,7 @@ class Model_creator
         $sql="SELECT employees_items_node.org_str_id FROM employees_items_node WHERE employees_items_node.employe_id =".  $_SESSION['employee_id'];
 //        $observer_org_str_id = $db->one($sql);
 //        $observer_org_str_id = 181   ;// Диспетчер автомобильного и городского наземного электротранспорта
-        $observer_org_str_id = 0   ;// показвать только отделу кадров
+        $observer_org_str_id = 166   ;// показвать только отделу кадров
         $sql = "INSERT INTO `local_alerts` (`observer_org_str_id`, `action_type_id`,`company_id`,`save_temp_files_id`,`date_create`)
                                        VALUES( '" .  $observer_org_str_id.
             "','" . $action_type_id .
